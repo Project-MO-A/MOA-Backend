502a78bfd4a20f69d461776c3cf85a52c6891c4a
JHS <wnghdtjr129@naver.com>
2023-03-10 21:05:32 +0900

    test: JWT 발행, 검증 테스트

---
+++ src/test/java/com/moa/global/auth/utils/JwtServiceTest.java
@@ -0,0 +1,151 @@
+package com.moa.global.auth.utils;
+
+import com.moa.global.auth.model.TokenMapping;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import java.util.Optional;
+
+import static java.lang.Thread.sleep;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class JwtServiceTest {
+
+    private static final String SECRET = "secret";
+    private static final long ACCESS_TOKEN_VALIDATION_SECONDS = 1;
+    private static final long REFRESH_TOKEN_VALIDATION_SECONDS = 1;
+    private static final String ACCESS_HEADER = "Authorization";
+    private static final String REFRESH_HEADER = "AuthorizationRefresh";
+
+    private final JwtService jwtService = new JwtService(
+            SECRET,
+            ACCESS_TOKEN_VALIDATION_SECONDS,
+            REFRESH_TOKEN_VALIDATION_SECONDS,
+            ACCESS_HEADER,
+            REFRESH_HEADER);
+
+    @Test
+    @DisplayName("토큰 생성")
+    void createToken() {
+        //given
+        String email = "user@email.com";
+
+        //when
+        TokenMapping token = jwtService.createToken(email);
+
+        //then
+        assertThat(token.accessToken()).isNotNull();
+        assertThat(token.refreshToken()).isNotNull();
+    }
+
+    @Test
+    @DisplayName("토큰 헤더 통해 전달")
+    void sendToken() {
+        //given
+        String email = "user@email.com";
+
+        //when
+        TokenMapping token = jwtService.createToken(email);
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        jwtService.sendBothToken(response, token.accessToken(), token.refreshToken());
+
+        //then
+        String accessToken = response.getHeader(ACCESS_HEADER);
+        String refreshToken = response.getHeader(REFRESH_HEADER);
+
+        assertThat(token.accessToken()).isEqualTo(accessToken);
+        assertThat(token.refreshToken()).isEqualTo(refreshToken);
+    }
+
+    @Test
+    @DisplayName("전달 받은 토큰 추출")
+    void extractToken() {
+        //given
+        String sendAccessToken = "Bearer accessToken";
+        String sendRefreshToken = "Bearer refreshToken";
+
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.addHeader(ACCESS_HEADER, sendAccessToken);
+        request.addHeader(REFRESH_HEADER, sendRefreshToken);
+
+        //when
+        Optional<String> accessToken = jwtService.extractAccessToken(request);
+        Optional<String> refreshToken = jwtService.extractRefreshToken(request);
+
+        //then
+        assertThat(accessToken.isPresent()).isTrue();
+        assertThat(accessToken.get()).isEqualTo(sendAccessToken.replace("Bearer ", ""));
+        assertThat(refreshToken.isPresent()).isTrue();
+        assertThat(refreshToken.get()).isEqualTo(sendRefreshToken.replace("Bearer ", ""));
+    }
+
+    @Test
+    @DisplayName("유효하지 않은 토큰 전달받으면 Optional.empty 반환")
+    void invalidToken() {
+        //given
+        String sendAccessToken = "invalid";
+        String sendRefreshToken = "invalid";
+
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.addHeader(ACCESS_HEADER, sendAccessToken);
+        request.addHeader(REFRESH_HEADER, sendRefreshToken);
+
+        //when
+        Optional<String> accessToken = jwtService.extractAccessToken(request);
+        Optional<String> refreshToken = jwtService.extractRefreshToken(request);
+
+        //then
+        assertThat(accessToken.isEmpty()).isTrue();
+        assertThat(refreshToken.isEmpty()).isTrue();
+    }
+
+    @Test
+    @DisplayName("유저 이메일 정보 확인")
+    void extractUserEmail() {
+        //given
+        String email = "user@email.com";
+        String accessToken = jwtService.createAccessToken(email);
+
+        //when
+        String extractEmail = jwtService.extractUserEmail(accessToken);
+
+        //then
+        assertThat(extractEmail).isEqualTo(email);
+    }
+
+    @Test
+    @DisplayName("유효 시간 지나지 않은 토큰 사용")
+    void useValidToken() {
+        //given
+        String email = "user@email.com";
+        TokenMapping token = jwtService.createToken(email);
+
+        //when
+        boolean isAccessTokenValid = jwtService.isTokenValid(token.accessToken().replace("Bearer ", ""));
+        boolean isRefreshTokenValid = jwtService.isTokenValid(token.refreshToken().replace("Bearer ", ""));
+
+        //then
+        assertThat(isAccessTokenValid).isTrue();
+        assertThat(isRefreshTokenValid).isTrue();
+    }
+
+    @Test
+    @DisplayName("유효 시간 지난 토큰 사용 검증 실패")
+    void useInvalidToken() throws InterruptedException {
+        //given
+        String email = "user@email.com";
+        TokenMapping token = jwtService.createToken(email);
+
+        //when
+        sleep(1000);
+        boolean isAccessTokenValid = jwtService.isTokenValid(token.accessToken().replace("Bearer ", ""));
+        boolean isRefreshTokenValid = jwtService.isTokenValid(token.refreshToken().replace("Bearer ", ""));
+
+        //then
+        assertThat(isAccessTokenValid).isFalse();
+        assertThat(isRefreshTokenValid).isFalse();
+    }
+
+}
