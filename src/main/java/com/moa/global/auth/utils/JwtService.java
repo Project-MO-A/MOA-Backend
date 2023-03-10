fc1005ec8804d7ad9486507fe5515af7a787393f
JHS <wnghdtjr129@naver.com>
2023-03-10 21:06:42 +0900

    feat: 생성자 추가
    추후 다른 구조로 refactoring 고려해 볼 것

--- src/main/java/com/moa/global/auth/utils/JwtService.java
+++ src/main/java/com/moa/global/auth/utils/JwtService.java
@@ -6,6 +6,7 @@ import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.moa.global.auth.model.TokenMapping;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +14,7 @@ import java.util.Date;
 import java.util.Optional;
 
 @Service
+@AllArgsConstructor
 public class JwtService {
 
     private final String PREFIX = "Bearer ";
