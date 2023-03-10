4f1fdbed785b7a847a3da1b78a36e73f42570d2f
JHS <wnghdtjr129@naver.com>
2023-03-10 16:10:30 +0900

    feat: bean validation 적용

--- src/main/java/com/moa/dto/user/UserSignupRequest.java
+++ src/main/java/com/moa/dto/user/UserSignupRequest.java
@@ -1,13 +1,15 @@
 package com.moa.dto.user;
 
 import com.moa.domain.user.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 
 @Builder
 public record UserSignupRequest(
-        String email,
-        String password,
-        String name,
+        @NotBlank @Email String email,
+        @NotBlank String password,
+        @NotBlank String name,
         String nickname,
         String details,
         double locationLatitude,
