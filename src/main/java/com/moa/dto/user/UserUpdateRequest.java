e506e0caac3540b832a3140b8e81fcaabf2034e7
KAispread <pain0121@naver.com>
2023-03-13 17:42:22 +0900

    refactor: validation 수정

--- src/main/java/com/moa/dto/user/UserUpdateRequest.java
+++ src/main/java/com/moa/dto/user/UserUpdateRequest.java
@@ -1,13 +1,17 @@
 package com.moa.dto.user;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 import lombok.Builder;
+import org.hibernate.validator.constraints.Length;
 
 @Builder
 public record UserUpdateRequest(
-        String password,
-        String name,
-        String nickname,
+        @Email @NotBlank String email,
+        @NotBlank String password,
+        @NotBlank @Length(min = 1, max = 15) String name,
+        @NotBlank @Length(min = 1, max = 15) String nickname,
         @Positive double locationLatitude,
         @Positive double locationLongitude,
         @Positive int popularity,
