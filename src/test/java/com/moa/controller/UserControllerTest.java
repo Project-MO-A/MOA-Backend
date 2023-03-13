f5895ee1df0e1ed2c9a8f25753fef167b358d9e8
KAispread <pain0121@naver.com>
2023-03-13 22:35:29 +0900

    test: 사용자 정보 조회, 수정 test

--- src/test/java/com/moa/controller/UserControllerTest.java
+++ src/test/java/com/moa/controller/UserControllerTest.java
@@ -1,10 +1,13 @@
 package com.moa.controller;
 
 import com.moa.base.AbstractControllerTest;
+import com.moa.dto.user.UserInfoResponse;
 import com.moa.dto.user.UserSignupRequest;
+import com.moa.dto.user.UserUpdateRequest;
 import com.moa.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -16,16 +19,15 @@ import org.springframework.test.web.servlet.ResultActions;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.payload.JsonFieldType.*;
-import static org.springframework.restdocs.payload.JsonFieldType.STRING;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -158,4 +160,159 @@ class UserControllerTest extends AbstractControllerTest {
         //then
         action.andExpect(status().isForbidden());
     }
+
+    @DisplayName("사용자 개인정보 관련 기능")
+    @Nested
+    class Information {
+        final String EMAIL = "test@email.com";
+        @BeforeEach
+        void setUp() {
+            //given
+            List<String> interests = new ArrayList<>();
+            interests.add("Java");
+            interests.add("Python");
+
+            UserSignupRequest request = UserSignupRequest.builder()
+                    .email(EMAIL)
+                    .password("password")
+                    .name("name")
+                    .nickname("John")
+                    .locationLongitude(34.14123)
+                    .locationLatitude(34.14123)
+                    .details("Hello")
+                    .interests(interests)
+                    .build();
+
+            userService.saveUser(request);
+        }
+
+        @Test
+        @DisplayName("getInfo - 사용자의 정보를 가져오는데 성공한다.")
+        @WithMockUser
+        void getInfo() throws Exception {
+            //when
+            ResultActions action = mvc.perform(get("/user/info?email="+EMAIL)
+                    .header(HttpHeaders.AUTHORIZATION, "access.jwt.token")
+                    .header("AuthorizationRefresh", "refresh.jwt.token"));
+
+            action.andExpectAll(
+                    status().isOk(),
+                    jsonPath("$.email").value(EMAIL),
+                    jsonPath("$.name").value("name"),
+                    jsonPath("$.nickname").value("John"),
+                    jsonPath("$.locationLatitude").value(34.14123),
+                    jsonPath("$.locationLongitude").value(34.14123),
+                    jsonPath("$.details").value("Hello"),
+                    jsonPath("$.interests.[0]").value("Java"),
+                    jsonPath("$.interests.[1]").value("Python")
+            );
+        }
+
+        @Test
+        @DisplayName("getInfo - 사용자의 정보를 가져오는데 실패한다. (잘못된 이메일)")
+        @WithMockUser
+        void getInfoFail1() throws Exception {
+            //when
+            String none = "none@email.com";
+            ResultActions action = mvc.perform(get("/user/info?email="+none)
+                    .header(HttpHeaders.AUTHORIZATION, "access.jwt.token")
+                    .header("AuthorizationRefresh", "refresh.jwt.token"));
+
+            //400
+            action.andExpect(status().isBadRequest());
+        }
+
+        @DisplayName("update - 사용자 정보를 수정하는데 성공한다.")
+        @WithMockUser
+        @Test
+        void update() throws Exception {
+            //given
+            List<String> interest = new ArrayList<>();
+            interest.add("AWS");
+            interest.add("Jenkins");
+
+            UserUpdateRequest request = UserUpdateRequest.builder()
+                    .email(EMAIL)
+                    .name("Jenny")
+                    .nickname("Honey")
+                    .locationLatitude(25.1234123)
+                    .locationLongitude(25.1234123)
+                    .details("Bye")
+                    .interests(interest)
+                    .build();
+
+            //when
+            ResultActions action = mvc.perform(put("/user/info")
+                    .content(toJson(request))
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .header(HttpHeaders.AUTHORIZATION, "access.jwt.token")
+                    .header("AuthorizationRefresh", "refresh.jwt.token"));
+
+            action.andExpect(status().isNoContent());
+            UserInfoResponse info = userService.getUserInfoByEmail(EMAIL);
+            assertThat(info.getName()).isEqualTo("Jenny");
+            assertThat(info.getNickname()).isEqualTo("Honey");
+            assertThat(info.getDetails()).isEqualTo("Bye");
+            assertThat(info.getInterests()).containsOnly("AWS", "Jenkins");
+        }
+
+        @DisplayName("update - 사용자 정보를 수정하는데 실패한다. (validation)")
+        @WithMockUser
+        @Test
+        void updateFail1() throws Exception {
+            //given
+            List<String> interest = new ArrayList<>();
+            interest.add("AWS");
+            interest.add("Jenkins");
+
+            UserUpdateRequest request = UserUpdateRequest.builder()
+                    .email(EMAIL)
+                    .name("Very very very very very very very very long name")
+                    .nickname("Honey")
+                    .locationLatitude(25.1234123)
+                    .locationLongitude(25.1234123)
+                    .details("Bye")
+                    .interests(interest)
+                    .build();
+
+            //when
+            ResultActions action = mvc.perform(put("/user/info")
+                    .content(toJson(request))
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .header(HttpHeaders.AUTHORIZATION, "access.jwt.token")
+                    .header("AuthorizationRefresh", "refresh.jwt.token"));
+
+            action.andExpectAll(status().isBadRequest());
+        }
+
+        @DisplayName("update - 사용자 정보를 수정하는데 실패한다. (잘못된 이메일)")
+        @WithMockUser
+        @Test
+        void updateFail2() throws Exception {
+            //given
+            List<String> interest = new ArrayList<>();
+            interest.add("AWS");
+            interest.add("Jenkins");
+
+            UserUpdateRequest request = UserUpdateRequest.builder()
+                    .email("none@email.com")
+                    .name("Very very very very very very very very long name")
+                    .nickname("Honey")
+                    .locationLatitude(25.1234123)
+                    .locationLongitude(25.1234123)
+                    .details("Bye")
+                    .interests(interest)
+                    .build();
+
+            //when
+            ResultActions action = mvc.perform(put("/user/info")
+                    .content(toJson(request))
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .header(HttpHeaders.AUTHORIZATION, "access.jwt.token")
+                    .header("AuthorizationRefresh", "refresh.jwt.token"));
+
+            action.andExpectAll(status().isBadRequest());
+        }
+    }
+
 }
