8b720d8fd04afa9cf18e011cf6c983f6572be76c
JHS <wnghdtjr129@naver.com>
2023-03-15 18:28:03 +0900

    test: 회원 참여 신청 서비스 로직 테스트

--- src/test/java/com/moa/base/AbstractServiceTest.java
+++ src/test/java/com/moa/base/AbstractServiceTest.java
@@ -1,17 +1,16 @@
 package com.moa.base;
 
+import com.moa.domain.member.ApplimentMemberRepository;
 import com.moa.domain.user.UserRepository;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @ExtendWith(MockitoExtension.class)
 public abstract class AbstractServiceTest {
 
     @Mock
     protected UserRepository userRepository;
-
     @Mock
-    protected PasswordEncoder passwordEncoder;
+    protected ApplimentMemberRepository applimentMemberRepository;
 }
