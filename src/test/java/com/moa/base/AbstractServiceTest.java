96101b314bfb2ad57d86029d4b075a3d89f264f5
JHS <wnghdtjr129@naver.com>
2023-03-17 20:42:51 +0900

    test: 유저 정보 값 형식 변경으로 인한 테스트 변경
    - 모집 글 기능 구현 시 테스트 코드 변경 필수

--- src/test/java/com/moa/base/AbstractServiceTest.java
+++ src/test/java/com/moa/base/AbstractServiceTest.java
@@ -6,6 +6,7 @@ import com.moa.domain.user.UserRepository;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @ExtendWith(MockitoExtension.class)
 public abstract class AbstractServiceTest {
@@ -14,6 +15,8 @@ public abstract class AbstractServiceTest {
     protected UserRepository userRepository;
     @Mock
     protected ApplimentMemberRepository applimentMemberRepository;
+    @Mock
+    protected PasswordEncoder passwordEncoder;
     @Mock
     protected RecruitMemberRepository recruitMemberRepository;
 }
