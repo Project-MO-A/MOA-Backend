2c55c1ba411a6502adbd21207f1a9ea27d7ad285
KAispread <pain0121@naver.com>
2023-03-18 23:55:47 +0900

    test: 카테고리 관련 기능 테스트

---
+++ src/test/java/com/moa/service/CategoryServiceTest.java
@@ -0,0 +1,59 @@
+package com.moa.service;
+
+import com.moa.domain.recruit.category.Category;
+import com.moa.domain.recruit.category.CategoryRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Transactional
+@SpringBootTest
+class CategoryServiceTest {
+    @Autowired
+    CategoryService categoryService;
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @BeforeEach
+    void setUp() {
+        categoryRepository.save(new Category("개발"));
+    }
+
+    @DisplayName("save - 카테고리 저장에 성공한다.")
+    @Test
+    void save1() {
+        //given
+        List<String> list = List.of("프로젝트", "웹");
+
+        //when
+        categoryService.save(list);
+
+        //then
+        List<Long> idList = categoryRepository.findIdByName(list);
+        assertThat(idList.get(0)).isGreaterThan(1);
+        assertThat(idList.size()).isEqualTo(2);
+    }
+
+    @DisplayName("save - 중복되는 카테고리를 제외하고 저장한다.")
+    @Test
+    void save2() {
+        //given
+        List<String> list = List.of("개발", "프로젝트", "웹");
+
+        //when
+        List<Category> save = categoryService.save(list);
+
+        //then
+        List<Long> idList = categoryRepository.findIdByName(list);
+        assertThat(idList.size()).isEqualTo(3);
+        assertThat(save.size()).isEqualTo(2);
+    }
+}
