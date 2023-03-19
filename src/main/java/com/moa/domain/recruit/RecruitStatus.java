c3226c196ef34aaa230f0ebf35bc8e649373abbc
KAispread <pain0121@naver.com>
2023-03-19 14:41:52 +0900

    refactor: 모집글 상태 구조 변경

---
+++ src/main/java/com/moa/domain/recruit/RecruitStatus.java
@@ -0,0 +1,26 @@
+package com.moa.domain.recruit;
+
+import lombok.Getter;
+
+import java.util.Arrays;
+
+@Getter
+public enum RecruitStatus {
+    RECRUITING(1, "모집중"),
+    COMPLETE(2, "모집완료");
+
+    private final int code;
+    private final String status;
+
+    RecruitStatus(int code, String status) {
+        this.code = code;
+        this.status = status;
+    }
+
+    public static RecruitStatus getState(int stateCode) {
+        return Arrays.stream(RecruitStatus.values())
+                .filter(state -> state.getCode() == stateCode)
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+}
