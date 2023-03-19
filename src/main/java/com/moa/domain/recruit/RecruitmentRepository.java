234cfdda7c955fd349b736f2f638223cf52244d4
KAispread <pain0121@naver.com>
2023-03-19 14:42:35 +0900

    feat: fetch join 쿼리 추가

--- src/main/java/com/moa/domain/recruit/RecruitmentRepository.java
+++ src/main/java/com/moa/domain/recruit/RecruitmentRepository.java
@@ -1,6 +1,12 @@
 package com.moa.domain.recruit;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface RecruitmentRepository extends JpaRepository<Recruitment, Long> {
+    @Query("select r from Recruitment r join fetch r.user where r.id = :id")
+    Optional<Recruitment> findByIdFetchUser(@Param("id") Long recruitId);
 }
