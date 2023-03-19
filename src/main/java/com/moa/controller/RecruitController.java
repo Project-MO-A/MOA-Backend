451ed7c7e62634641e7bee7613ce02ac66abfb38
KAispread <pain0121@naver.com>
2023-03-19 14:43:14 +0900

    feat: 모집글 관련 기능 service, controller 구현

---
+++ src/main/java/com/moa/controller/RecruitController.java
@@ -0,0 +1,49 @@
+package com.moa.controller;
+
+import com.moa.dto.recruit.RecruitInfoResponse;
+import com.moa.dto.recruit.RecruitPostRequest;
+import com.moa.dto.recruit.RecruitUpdateRequest;
+import com.moa.service.CategoryService;
+import com.moa.service.RecruitService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/recruit")
+@RequiredArgsConstructor
+@RestController
+public class RecruitController {
+    private final RecruitService recruitService;
+    private final CategoryService categoryService;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public Long post(@RequestBody @Valid RecruitPostRequest request, Long userId) {
+        List<Long> categoryId = categoryService.updateAndReturnId(request.category());
+        return recruitService.post(userId, request, categoryId);
+    }
+
+    @GetMapping("/{recruitId}")
+    public RecruitInfoResponse info(@PathVariable Long recruitId) {
+        return recruitService.getInfo(recruitId);
+    }
+
+    @PatchMapping("/{recruitId}")
+    public Long updatePost(@PathVariable Long recruitId, @RequestBody @Valid RecruitUpdateRequest request) {
+        List<Long> categoryId = categoryService.updateAndReturnId(request.category());
+        return recruitService.update(recruitId, request, categoryId);
+    }
+
+    @PostMapping("/{recruitId}")
+    public Long updateStatus(@PathVariable Long recruitId, @RequestParam Integer statusCode) {
+        return recruitService.updateStatus(recruitId, statusCode);
+    }
+
+    @DeleteMapping("/{recruitId}")
+    public Long delete(@PathVariable Long recruitId) {
+        return recruitService.delete(recruitId);
+    }
+}
--- src/main/java/com/moa/service/RecruitService.java
+++ src/main/java/com/moa/service/RecruitService.java
@@ -1,16 +1,17 @@
 package com.moa.service;
 
-import com.moa.domain.member.RecruitMember;
 import com.moa.domain.recruit.Recruitment;
 import com.moa.domain.recruit.RecruitmentRepository;
 import com.moa.domain.recruit.category.CategoryRepository;
 import com.moa.domain.recruit.category.RecruitCategory;
 import com.moa.domain.user.User;
 import com.moa.domain.user.UserRepository;
+import com.moa.dto.recruit.RecruitInfoResponse;
 import com.moa.dto.recruit.RecruitPostRequest;
-import jakarta.transaction.Transactional;
+import com.moa.dto.recruit.RecruitUpdateRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -24,21 +25,40 @@ public class RecruitService {
 
     public Long post(final Long userId, final RecruitPostRequest request, final List<Long> categoryId) {
         User user = userRepository.getReferenceById(userId);
-        Recruitment recruitment = request.toEntity(user);
-        setAssociation(request, categoryId, recruitment);
+        Recruitment recruitment = request.toEntity(user, request.toMemberList(), getRecruitCategories(categoryId));
         return recruitmentRepository.save(recruitment).getId();
     }
 
-    private void setAssociation(RecruitPostRequest request, List<Long> categoryId, Recruitment recruitment) {
-        List<RecruitMember> members = request.memberFields().stream().map(field -> new RecruitMember(field.field(), field.total())).toList();
-        List<RecruitCategory> list = categoryId.stream()
+    @Transactional(readOnly = true)
+    public RecruitInfoResponse getInfo(final Long recruitId) {
+        Recruitment recruitment = recruitmentRepository.findByIdFetchUser(recruitId).orElseThrow(IllegalArgumentException::new);
+        return new RecruitInfoResponse(recruitment);
+    }
+
+    public Long update(final Long recruitId, final RecruitUpdateRequest request, final List<Long> categoryId) {
+        Recruitment recruitment = recruitmentRepository.findById(recruitId).orElseThrow(IllegalArgumentException::new);
+        recruitment.update(request, getRecruitCategories(categoryId));
+        return recruitment.getId();
+    }
+
+    public Long updateStatus(final Long recruitId, final Integer statusCode) {
+        Recruitment recruitment = recruitmentRepository.findById(recruitId).orElseThrow(IllegalArgumentException::new);
+        recruitment.updateState(statusCode);
+        return recruitment.getId();
+    }
+
+    public Long delete(final Long recruitId) {
+        recruitmentRepository.findById(recruitId).orElseThrow(IllegalArgumentException::new);
+        recruitmentRepository.deleteById(recruitId);
+        return recruitId;
+    }
+
+    private List<RecruitCategory> getRecruitCategories(List<Long> categoryId) {
+        return categoryId.stream()
                 .map(categoryRepository::getReferenceById)
                 .toList()
                 .stream()
                 .map(RecruitCategory::new)
                 .toList();
-
-        recruitment.setMembers(members);
-        recruitment.setCategory(list);
     }
 }
