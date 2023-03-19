dac8b9e972a6463e0f08158dd15e791bfe80dd8f
KAispread <pain0121@naver.com>
2023-03-19 14:42:15 +0900

    refactor: 중간 테이블 getter 추가

--- src/main/java/com/moa/domain/recruit/category/RecruitCategory.java
+++ src/main/java/com/moa/domain/recruit/category/RecruitCategory.java
@@ -2,6 +2,8 @@ package com.moa.domain.recruit.category;
 
 import com.moa.domain.recruit.Recruitment;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.domain.Persistable;
@@ -9,10 +11,11 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
 
-@NoArgsConstructor
-@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EntityListeners(AuditingEntityListener.class)
 @Table(name = "RECRUIT_CATEGORY")
+@Entity
 public class RecruitCategory implements Persistable<RecruitCategoryId> {
     @EmbeddedId
     private RecruitCategoryId id;
