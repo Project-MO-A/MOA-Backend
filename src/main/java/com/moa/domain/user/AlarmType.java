1b649c35b2ef9dacd61d4f1bf8b274886592ea31
KAispread <pain0121@naver.com>
2023-03-22 19:21:54 +0900

    refactor: String.format 을 사용하도록 변경

--- src/main/java/com/moa/domain/user/AlarmType.java
+++ src/main/java/com/moa/domain/user/AlarmType.java
@@ -1,18 +1,18 @@
 package com.moa.domain.user;
 
 public enum AlarmType {
-    RECRUITMENT_COMPLETE("{id}번 모집글에 대한 인원 모집이 완료되었습니다.", "/recruitment/{id}"),
-    PARTICIPATION_REQUEST("{id}번 모집글에 대한 새로운 참여 요청이 등록되었습니다.", "/recruitment/{id}/users"),
-    PARTICIPATION_APPROVAL("{id}번 모집글에 대한 참여 요청이 승인되었습니다.", "/recruitment/{id}"),
-    PARTICIPATION_REJECT("{id}번 모집글에 대한 참여 요청이 거절되었습니다.", "/recruitment/{id}"),
+    RECRUITMENT_COMPLETE("%d번 모집글에 대한 인원 모집이 완료되었습니다.", "/recruitment/%d"),
+    PARTICIPATION_REQUEST("%d번 모집글에 대한 새로운 참여 요청이 등록되었습니다.", "/recruitment/%d/users"),
+    PARTICIPATION_APPROVAL("%d번 모집글에 대한 참여 요청이 승인되었습니다.", "/recruitment/%d"),
+    PARTICIPATION_REJECT("%d번 모집글에 대한 참여 요청이 거절되었습니다.", "/recruitment/%d"),
 
-    NOTICE_POST("공지사항이 등록되었습니다.", "/notice/{id}"),
+    NOTICE_POST("공지사항이 등록되었습니다.", "/notice/%d"),
 
-    TIME_POST("{id}번 공지사항에 대한 시간 조율이 등록되었습니다.", "/notice/{id}/time"),
-    TIME_COMPLETE("{id}번 공지사항에 대한 시간 조율이 완료되었습니다.", "/notice/{id}/time"),
+    TIME_POST("%d번 공지사항에 대한 시간 조율이 등록되었습니다.", "/notice/%d/time"),
+    TIME_COMPLETE("%d번 공지사항에 대한 시간 조율이 완료되었습니다.", "/notice/%d/time"),
 
-    PLACE_POST("{id}번 공지사항에 대한 장소 조율이 등록되었습니다.", "/notice/{id}/place"),
-    PLACE_COMPLETE("{id}번 공지사항에 대한 장소 조율이 완료되었습니다.", "/notice/{id}/place");
+    PLACE_POST("%d번 공지사항에 대한 장소 조율이 등록되었습니다.", "/notice/%d/place"),
+    PLACE_COMPLETE("%d번 공지사항에 대한 장소 조율이 완료되었습니다.", "/notice/%d/place");
 
     private final String message;
     private final String uri;
@@ -23,10 +23,10 @@ public enum AlarmType {
     }
 
     public String getRedirectURI(Long id) {
-        return this.uri.replaceFirst("\\{id}", String.valueOf(id));
+        return String.format(this.uri, id);
     }
 
     public String getMessage(Long id) {
-        return this.message.replaceFirst("\\{id}", String.valueOf(id));
+        return String.format(this.message, id);
     }
 }
