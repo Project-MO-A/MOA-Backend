bb0bd53edd1d5450c1121f86d23076e8908781f3
LeeKiWoo <99247279+KAispread@users.noreply.github.com>
2023-03-24 22:26:45 +0900

    refactor: 신청 멤버 상수값 변경

--- src/main/java/com/moa/domain/member/Approval.java
+++ src/main/java/com/moa/domain/member/Approval.java
@@ -1,6 +1,30 @@
 package com.moa.domain.member;
 
+import com.moa.global.exception.service.InvalidCodeException;
+import lombok.Getter;
+
+import java.util.Arrays;
+
+import static com.moa.global.exception.ErrorCode.STATUS_CODE_INVALID;
+
+@Getter
 public enum Approval {
-    APPROVED,
-    PENDING
+    PENDING(1, "대기중"),
+    APPROVED(2, "승인"),
+    REFUSE(3, "거절");
+
+    private final int code;
+    private final String status;
+
+    Approval(int code, String status) {
+        this.code = code;
+        this.status = status;
+    }
+
+    public static Approval getInstance(int code) {
+        return Arrays.stream(Approval.values())
+                .filter(a -> a.code == code)
+                .findFirst()
+                .orElseThrow(() -> new InvalidCodeException(STATUS_CODE_INVALID));
+    }
 }
