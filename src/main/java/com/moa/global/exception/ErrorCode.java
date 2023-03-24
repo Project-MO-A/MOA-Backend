7848e43560d9cf5dfea9f7adec2c1db6ef965c17
LeeKiWoo <99247279+KAispread@users.noreply.github.com>
2023-03-24 22:25:32 +0900

    feat: 상태 코드 예외 추가

--- src/main/java/com/moa/global/exception/ErrorCode.java
+++ src/main/java/com/moa/global/exception/ErrorCode.java
@@ -5,6 +5,8 @@ public enum ErrorCode {
     JWT_NOT_VALID(400, "token.not.valid", "A0003"),
     LOGIN_BAD_VALUE(401, "login.bad.value", "A0004"),
 
+    STATUS_CODE_INVALID(400, "status.code.invalid", "S0001"),
+
     USER_NOT_FOUND(404, "user.not.found", "U0001"),
     DUPLICATED_EMAIL(400, "user.duplicate.email", "U0002"),
     USER_MISMATCH_PASSWORD(400, "user.mismatch.password", "U0003"),
