f4fde03cab8e3873f665f07979fb9e5fb8857de2
JHS <wnghdtjr129@naver.com>
2023-03-25 20:54:07 +0900

    feat: DB변경으로 인한 엔티티 재정의

---
+++ src/main/java/com/moa/domain/member/AttendMemberRepository.java
@@ -0,0 +1,15 @@
+package com.moa.domain.member;
+
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Collection;
+import java.util.List;
+
+import static org.springframework.data.jpa.repository.EntityGraph.EntityGraphType.LOAD;
+
+public interface AttendMemberRepository extends JpaRepository<AttendMember, Long> {
+
+    @EntityGraph(attributePaths = {"user"}, type = LOAD)
+    List<AttendMember> findAllByNoticeIdIn(Collection<Long> noticeId);
+}
--- src/main/java/com/moa/domain/notice/NoticeRepository.java
+++ src/main/java/com/moa/domain/notice/NoticeRepository.java
@@ -2,5 +2,8 @@ package com.moa.domain.notice;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
+    List<Notice> findAllByRecruitmentId(Long recruitmentId);
 }
