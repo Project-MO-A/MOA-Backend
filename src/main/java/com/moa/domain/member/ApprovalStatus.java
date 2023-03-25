584d30b6a18a513dcb82e445f79a05aa1a47c8fb
LeeKiWoo <99247279+KAispread@users.noreply.github.com>
2023-03-25 14:15:12 +0900

    refactor: 신청 상태에 '강퇴' 추가

--- src/main/java/com/moa/domain/member/ApprovalStatus.java
+++ src/main/java/com/moa/domain/member/ApprovalStatus.java
@@ -1,5 +1,6 @@
 package com.moa.domain.member;
 
+import com.fasterxml.jackson.annotation.JsonValue;
 import com.moa.global.exception.service.InvalidCodeException;
 import lombok.Getter;
 
@@ -8,21 +9,23 @@ import java.util.Arrays;
 import static com.moa.global.exception.ErrorCode.STATUS_CODE_INVALID;
 
 @Getter
-public enum Approval {
+public enum ApprovalStatus {
     PENDING(1, "대기중"),
     APPROVED(2, "승인"),
-    REFUSE(3, "거절");
+    REFUSE(3, "거절"),
+    KICK(4, "강퇴");
 
     private final int code;
+    @JsonValue
     private final String status;
 
-    Approval(int code, String status) {
+    ApprovalStatus(int code, String status) {
         this.code = code;
         this.status = status;
     }
 
-    public static Approval getInstance(int code) {
-        return Arrays.stream(Approval.values())
+    public static ApprovalStatus getStatus(int code) {
+        return Arrays.stream(ApprovalStatus.values())
                 .filter(a -> a.code == code)
                 .findFirst()
                 .orElseThrow(() -> new InvalidCodeException(STATUS_CODE_INVALID));
