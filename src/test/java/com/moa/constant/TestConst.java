4cc1bf138c98f7ab424570c012023318bc5f1a73
JHS <wnghdtjr129@naver.com>
2023-03-27 22:21:39 +0900

    test: test 상수, mock 객체 생성

--- src/test/java/com/moa/constant/TestConst.java
+++ src/test/java/com/moa/constant/TestConst.java
@@ -1,5 +1,9 @@
 package com.moa.constant;
 
+import com.moa.domain.interests.RecruitmentInterest;
+import com.moa.domain.notice.Post;
+import com.moa.domain.recruit.RecruitStatus;
+import com.moa.domain.recruit.Recruitment;
 import com.moa.domain.user.User;
 
 public class TestConst {
@@ -15,4 +19,23 @@ public class TestConst {
             .popularity(0)
             .details("details")
             .build();
+
+    public static final User ASSOCIATION_USER;
+
+    static {
+        ASSOCIATION_USER = User.builder()
+                .email("user@email.com")
+                .password("password")
+                .name("name")
+                .nickname("nickname")
+                .locationLatitude(23.23)
+                .locationLongitude(34.3443)
+                .popularity(0)
+                .details("details")
+                .build();
+        ASSOCIATION_USER
+                .addRecruitmentInterests(new RecruitmentInterest(ASSOCIATION_USER, new Recruitment(USER,
+                        new Post("title", "content"), RecruitStatus.RECRUITING))
+                );
+    }
 }
