c460d2acb32ac1281ae03673b3ab45343426cd6b
JHS <wnghdtjr129@naver.com>
2023-03-28 16:59:21 +0900

    feat: 그라함 알고리즘 service로 분리
    test -> service

---
+++ src/main/java/com/moa/service/graham/GrahamAlgo.java
@@ -0,0 +1,58 @@
+package com.moa.service.graham;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+import static java.util.Comparator.comparingDouble;
+
+public class GrahamAlgo {
+    public List<Point> getOutSide(List<Point> points) {
+        points.sort((o1, o2) -> o1.x == o2.x ? Double.compare(o1.y, o2.y) : Double.compare(o1.x, o2.x));
+
+        Point start = points.get(0);
+        points.remove(start);
+        points.forEach(point -> point.calculateTan(start));
+        points.sort(comparingDouble(o -> o.tan));
+
+        Stack<Point> stack = getOutSidePoints(points, start);
+        List<Point> result = new ArrayList<>();
+        while (!stack.isEmpty()) {
+            result.add(stack.pop());
+        }
+        return result;
+    }
+
+    private Stack<Point> getOutSidePoints(List<Point> points, Point start) {
+        Stack<Point> stack = new Stack<>();
+        stack.push(start);
+        stack.push(points.get(0));
+
+        for (int i = 1; i < points.size(); i++) {
+            while (!ccw(stack.get(stack.size() - 2), stack.get(stack.size() - 1), points.get(i))) {
+                stack.pop();
+            }
+            stack.push(points.get(i));
+        }
+        return stack;
+    }
+
+    private boolean ccw(Point first, Point second, Point third) {
+        return first.x * (second.y - third.y) + second.x * (third.y - first.y) + third.x * (first.y - second.y) > 0.0;
+    }
+
+    class Point {
+        private double x;
+        private double y;
+        private double tan;
+
+        public Point(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void calculateTan(Point start) {
+            this.tan = (this.y - start.y) / (this.x - start.x);
+        }
+    }
+}
