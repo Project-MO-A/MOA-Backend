4afe09daa4324a918f7cbf822a95ab2db2d8452f
JHS <wnghdtjr129@naver.com>
2023-03-28 13:32:58 +0900

    refactor: request json body 시간 타입 변경
    - String -> LocalDateTime

--- src/main/java/com/moa/dto/notice/PostNoticeRequest.java
+++ src/main/java/com/moa/dto/notice/PostNoticeRequest.java
@@ -5,15 +5,14 @@ import com.moa.domain.notice.Post;
 import com.moa.domain.recruit.Recruitment;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 public record PostNoticeRequest(
         @NotBlank String title,
         @NotBlank String content,
-        @NotBlank @Pattern(regexp = "^(\\d{4}-\\d{2}-\\d{2}\\s\\d{2}:\\d{2}:\\d{2})$") String meetingTime,
+        @NotBlank @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss") LocalDateTime meetingTime,
         String confirmedLocation,
         @NotNull Boolean checkVote
 ) {
@@ -21,7 +20,7 @@ public record PostNoticeRequest(
         return Notice.builder()
                 .recruitment(recruitment)
                 .post(new Post(title, content))
-                .confirmedTime(LocalDateTime.parse(meetingTime, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
+                .confirmedTime(meetingTime)
                 .confirmedLocation(confirmedLocation)
                 .checkVote(checkVote)
                 .build();
