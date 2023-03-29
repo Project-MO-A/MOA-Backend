4852e34255324a52b34f8ea51702f69303072798
LeeKiWoo <99247279+KAispread@users.noreply.github.com>
2023-03-29 23:33:34 +0900

    refactor: List 대신 Collection 을 받도록 변경

--- src/main/java/com/moa/domain/recruit/tag/TagRepository.java
+++ src/main/java/com/moa/domain/recruit/tag/TagRepository.java
@@ -4,12 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
     @Query("select t.name from Tag t where t.name in :names")
-    List<String> findExistName(@Param(value = "names") List<String> tags);
+    List<String> findExistName(@Param(value = "names") Collection<String> tags);
 
     @Query("select t.id from Tag t where t.name in :names")
-    List<Long> findIdByName(@Param(value = "names") List<String> tags);
+    List<Long> findIdByName(@Param(value = "names") Collection<String> tags);
 }
