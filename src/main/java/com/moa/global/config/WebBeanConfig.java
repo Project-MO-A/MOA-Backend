5ace2c0480579293907a6be2d841e594df6c4d5b
JHS <wnghdtjr129@naver.com>
2023-03-30 15:31:16 +0900

    refactor: Token 생성 클래스 분리
    Test 할때 Mock 객체 유지 및 조건 부여 어려움 해결 위함

--- src/main/java/com/moa/global/config/WebBeanConfig.java
+++ src/main/java/com/moa/global/config/WebBeanConfig.java
@@ -1,11 +1,13 @@
 package com.moa.global.config;
 
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.MessageSourceAccessor;
 
 @Configuration
+@ConfigurationPropertiesScan("com.moa")
 public class WebBeanConfig {
     @Bean
     public MessageSourceAccessor messageSourceAccessor(MessageSource messageSource) {
