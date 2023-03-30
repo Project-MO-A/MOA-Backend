2aefe6c5f60835e840b9b55df0d117f3b97cef18
LeeKiWoo <99247279+KAispread@users.noreply.github.com>
2023-03-30 23:28:07 +0900

    test: RepositoryTest 클래스 추가, TestQueryDsl 설정 추가

--- src/test/java/com/moa/base/RepositoryTest.java
+++ src/test/java/com/moa/base/RepositoryTest.java
@@ -1,8 +1,10 @@
-package com.moa.domain;
+package com.moa.base;
 
 
+import com.moa.support.config.TestQueryDslConfig;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -14,6 +16,7 @@ import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTest
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @AutoConfigureTestDatabase(replace = NONE)
+@Import(TestQueryDslConfig.class)
 @DataJpaTest
 public @interface RepositoryTest {
 }
