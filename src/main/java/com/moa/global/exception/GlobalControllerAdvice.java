e553b3b3f56b5a2f8ea404bbe6b30e30f1bf5964
LeeKiWoo <99247279+KAispread@users.noreply.github.com>
2023-04-01 11:50:54 +0900

    feat: 예외 처리 기능 추가 - @JsonFormat 바인딩 Exception Handler 추가 - NotNull 제약 조건 추가

--- src/main/java/com/moa/global/exception/GlobalControllerAdvice.java
+++ src/main/java/com/moa/global/exception/GlobalControllerAdvice.java
@@ -6,9 +6,12 @@ import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @Slf4j
 @RestControllerAdvice
@@ -35,5 +38,12 @@ public class GlobalControllerAdvice {
         HttpStatusCode statusCode = e.getStatusCode();
         log.error(LOG_FORMAT, e.getClass(), statusCode.value(), message);
         return new ResponseEntity<>(new ErrorResponse(String.valueOf(statusCode.value()), message), statusCode);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> httpMessageExceptionHandler(HttpMessageNotReadableException e) {
+        int code = BAD_REQUEST.value();
+        log.error(LOG_FORMAT, e.getClass(), code,  e.getMessage());
+        return new ResponseEntity<>(new ErrorResponse(String.valueOf(code), e.getMessage()), BAD_REQUEST);
     }
 }
