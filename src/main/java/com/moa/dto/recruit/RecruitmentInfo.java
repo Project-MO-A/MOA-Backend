5c43dea923f35974ad828c9e6adae832825096d3
LeeKiWoo <99247279+KAispread@users.noreply.github.com>
2023-04-09 20:43:10 +0900

    refactor: 모집글 조회 데이터 추가

--- src/main/java/com/moa/dto/recruit/RecruitmentInfo.java
+++ src/main/java/com/moa/dto/recruit/RecruitmentInfo.java
@@ -1,5 +1,6 @@
 package com.moa.dto.recruit;
 
+import com.moa.domain.member.RecruitMember;
 import com.moa.domain.recruit.Recruitment;
 import lombok.Getter;
 
@@ -10,23 +11,51 @@ import java.util.List;
 public class RecruitmentInfo {
     protected final Long id;
     protected final String title;
+    protected final String author;
     protected final LocalDateTime createdDate;
     protected final String recruitStatus;
     protected final String category;
     protected final List<String> tags;
+    protected final int totalCount;
+    protected final int approvedCount;
+    protected final int replyCount;
 
     public RecruitmentInfo(Recruitment recruitment) {
         this.id = recruitment.getId();
         this.title = recruitment.getPost().getTitle();
+        this.author = recruitment.getUser().getNickname();
         this.createdDate = recruitment.getCreatedDate();
         this.recruitStatus = recruitment.getStatus().getStatus();
         this.category = recruitment.getCategory().getName();
         this.tags = getTagNames(recruitment);
+        this.totalCount = getTotal(recruitment);
+        this.approvedCount = getApproved(recruitment);
+
+        // TO DO
+        this.replyCount = 0;
     }
 
     private List<String> getTagNames(Recruitment recruitment) {
         return recruitment.getTags().stream()
                 .map(recruitTag -> recruitTag.getTag().getName())
                 .toList();
+    }
+
+    private int getTotal(Recruitment recruitment) {
+        int count = 0;
+
+        for (RecruitMember recruitMember : recruitment.getMembers()) {
+            count += recruitMember.getTotalRecruitCount();
+        }
+        return count;
+    }
+
+    private int getApproved(Recruitment recruitment) {
+        int count = 0;
+
+        for (RecruitMember recruitMember : recruitment.getMembers()) {
+            count += recruitMember.getCurrentRecruitCount();
+        }
+        return count;
     }
 }
