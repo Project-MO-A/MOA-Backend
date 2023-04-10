17e56b113022c519accf9f788f4251bf1648b897
LeeKiWoo <99247279+KAispread@users.noreply.github.com>
2023-04-10 11:40:34 +0900

    feat: QueryDsl Projection용 생성자 추가

--- src/main/java/com/moa/dto/recruit/RecruitmentInfo.java
+++ src/main/java/com/moa/dto/recruit/RecruitmentInfo.java
@@ -1,6 +1,8 @@
 package com.moa.dto.recruit;
 
 import com.moa.domain.member.RecruitMember;
+import com.moa.domain.recruit.Category;
+import com.moa.domain.recruit.RecruitStatus;
 import com.moa.domain.recruit.Recruitment;
 import lombok.Getter;
 
@@ -15,7 +17,7 @@ public class RecruitmentInfo {
     protected final LocalDateTime createdDate;
     protected final String recruitStatus;
     protected final String category;
-    protected final List<String> tags;
+    protected List<String> tags;
     protected final int totalCount;
     protected final int approvedCount;
     protected final int replyCount;
@@ -33,6 +35,24 @@ public class RecruitmentInfo {
 
         // TO DO
         this.replyCount = 0;
+    }
+
+    public RecruitmentInfo(Long id, String title, String author, LocalDateTime createdDate, RecruitStatus recruitStatus, Category category, int totalCount, int approvedCount) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.createdDate = createdDate;
+        this.recruitStatus = recruitStatus.getStatus();
+        this.category = category.getName();
+        this.totalCount = totalCount;
+        this.approvedCount = approvedCount;
+
+        // TO DO
+        this.replyCount = 0;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
     }
 
     private List<String> getTagNames(Recruitment recruitment) {
