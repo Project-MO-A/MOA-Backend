d459aeff7537d5e87e0189b6f3aa7ee0eecfab64
KAispread <pain0121@naver.com>
2023-04-13 20:12:58 +0900

    feat: ID로 댓글 조회(user fetch) 기능 추가

--- src/main/java/com/moa/domain/reply/ReplyRepository.java
+++ src/main/java/com/moa/domain/reply/ReplyRepository.java
@@ -4,8 +4,12 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
     @EntityGraph(attributePaths = {"user"})
     List<Reply> findByRecruitmentIdOrderByParentIdAsc(Long recruitmentId);
+
+    @EntityGraph(attributePaths = {"user"})
+    Optional<Reply> findFetchUserById(Long id);
 }
--- src/test/java/com/moa/domain/reply/ReplyRepositoryTest.java
+++ src/test/java/com/moa/domain/reply/ReplyRepositoryTest.java
@@ -7,6 +7,8 @@ import com.moa.domain.recruit.Recruitment;
 import com.moa.domain.recruit.RecruitmentRepository;
 import com.moa.domain.user.User;
 import com.moa.domain.user.UserRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,6 +17,7 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
@@ -31,6 +34,9 @@ class ReplyRepositoryTest {
 
     @Autowired
     private UserRepository userRepository;
+
+    @PersistenceContext
+    EntityManager em;
 
     private Recruitment recruitment;
     private User user;
@@ -63,5 +69,21 @@ class ReplyRepositoryTest {
         assertThat(repliesResponse.size()).isEqualTo(5);
         assertThat(repliesResponse.get(0).getParentId()).isNull();
         assertThat(repliesResponse.get(2).getParentId()).isEqualTo(1L);
+    }
+
+    @DisplayName("아이디로 유저 데이터와 함께 댓글 데이터를 조회한다.")
+    @Test
+    void findFetchUserById() {
+        //given
+        Reply reply = replyRepository.save(new Reply("reply", null, user, recruitment));
+        final Long replyId = reply.getId();
+        em.flush();
+        em.clear();
+
+        //when
+        Optional<Reply> fetchUserById = replyRepository.findFetchUserById(replyId);
+
+        //then
+        assertThat(fetchUserById).isNotEmpty();
     }
 }
