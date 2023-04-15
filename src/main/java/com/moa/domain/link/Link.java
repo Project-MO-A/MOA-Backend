51081833f565213ce63a38549557dee88009bac1
JHS <wnghdtjr129@naver.com>
2023-04-15 11:03:25 +0900

    feat: Link entity mapping

---
+++ src/main/java/com/moa/domain/link/Link.java
@@ -0,0 +1,29 @@
+package com.moa.domain.link;
+
+import com.moa.domain.user.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name = "USER_LINK")
+@Getter
+@NoArgsConstructor
+public class Link {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    private String url;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_ID")
+    private User user;
+
+    public Link(String url, User user) {
+        this.url = url;
+        this.user = user;
+    }
+}
