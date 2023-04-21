14832950b169dc0f0e7767cbae21fd1152f4d061
KAispread <pain0121@naver.com>
2023-04-21 22:17:15 +0900

    feat: 모집글 조회 쿼리 추가

--- src/main/java/com/moa/domain/member/AdminRepository.java
+++ src/main/java/com/moa/domain/member/AdminRepository.java
@@ -1,5 +1,6 @@
 package com.moa.domain.member;
 
+import com.moa.domain.recruit.Recruitment;
 import com.moa.dto.member.ApplimentMemberResponse;
 import com.moa.dto.member.ApprovedMemberResponse;
 import com.querydsl.core.types.Projections;
@@ -79,6 +80,14 @@ public class AdminRepository implements ApplimentSearchRepository {
         }
 
         return approvedMemberResponses;
+    }
+
+    @Override
+    public Optional<Recruitment> findRecruitmentById(final Long recruitmentId) {
+        return Optional.ofNullable(queryFactory
+                .selectFrom(recruitment)
+                .where(recruitment.id.eq(recruitmentId))
+                .fetchOne());
     }
 
     public Long countAttend(final Long recruitmentId, final Long userId, final Attendance attendance) {
