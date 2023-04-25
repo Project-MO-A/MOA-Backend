27df78bc644b30585cf4c9b9bcddd66da5240f32
LeeKiWoo <99247279+KAispread@users.noreply.github.com>
2023-04-25 16:57:12 +0900

    feat: 어드민 참여/불참 상태 변경 기능 추가

--- src/main/java/com/moa/service/AttendMemberService.java
+++ src/main/java/com/moa/service/AttendMemberService.java
@@ -2,6 +2,7 @@ package com.moa.service;
 
 import com.moa.domain.member.AttendMember;
 import com.moa.domain.member.AttendMemberRepository;
+import com.moa.domain.member.Attendance;
 import com.moa.dto.notice.VoteAttendanceRequest;
 import com.moa.global.exception.service.EntityNotFoundException;
 import com.moa.global.exception.service.VoteFinishException;
@@ -29,5 +30,13 @@ public class AttendMemberService {
         attendMember.checkRecruitment(request.recruitmentId());
         attendMember.changeAttendance(request.attendance());
         return attendMember.getId();
+    }
+
+    public Attendance changeMemberAttendance(final Long attendMemberId, final Attendance attendance) {
+        AttendMember attendMember = attendMemberRepository.findById(attendMemberId)
+                .orElseThrow(() -> new EntityNotFoundException(ATTENDMEMBER_NOT_FOUND));
+
+        attendMember.changeAttendance(attendance);
+        return attendMember.getAttendance();
     }
 }
